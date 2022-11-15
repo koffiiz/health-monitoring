e4640961a9ff5c448526e374b482774f9efa46f5
devvydeve <jveecarabbacan@gmail.com>
2022-11-15 19:21:10 +0800

    Title Changed

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Health Monitoring') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
@@ -37,6 +37,7 @@
                     jQuery('#'+current).hide();
                 }); 
             });
+
         </script>
     </body>
 </html>
