a7b84da0aa7ff4c5db6fd4aae0c688a053e02d2d
devvydeve <jveecarabbacan@gmail.com>
2022-11-15 19:25:19 +0800

    Title Changed

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -38,6 +38,16 @@
                 }); 
             });
 
+//             jQuery(document).ready(function () {
+//                 jQuery("#eye").click(function () {
+//             if (jQuery("#password").attr("type") === "password") {
+//                 jQuery("#password").attr("type", "text");
+//             } else {
+//                 jQuery("#password").attr("type", "password");
+//             }
+//         });
+//   });
+
         </script>
     </body>
 </html>
