71e3097668fb0805f1e21cf65976ccb42d5e8d9f
Jude <judematthewmateo31@gmail.com>
2022-11-15 19:34:38 +0800

    title update

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Health Tracker') }}</title>
+        <title>Health Tracker</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
